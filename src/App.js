95d50e827e3a8ab29bb92aa01f008f2893d60395
Darpanarayan Bahadur <darpan.bahadur.2001@gmail.com>
2022-06-25 18:18:15 +0530

    removed readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import fire from './fire';
 import './App.css';
+import { Tracker } from './components/Tracker';
+import { Logsign } from "./components/Logsign";
 
 function App() {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const Login = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email,password)
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const message = errorMessage.split(":");
+        const message2 = message[1].split("(");
+        setError(message2[0]);
+      });
+  }
+
+  const signup = () =>{
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const message = errorMessage.split(":");
+        const message2 = message[1].split("(");
+        setError(message2[0]);
+      });
+  }
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user)=>{
+      if(user){
+          setUser(user);
+      }
+      else{
+        setUser("");
+      }
+    });
+  }
+
+  useEffect(() => {
+    authListener();
+  }, [user]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    { user ? (
+        <Tracker />
+      ):(
+        <Logsign 
+          email={email} 
+          setEmail={setEmail} 
+          password={password}
+          hasAccount={hasAccount}
+          setHasAccount={setHasAccount}
+          setPassword={setPassword} 
+          Login={Login}
+          signup={signup}
+          error={error}
+        />
+      )
+    }
+      
+    </>
   );
 }
 
---
+++ src/components/Tracker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Tracker = () => {
+  return (
+    <div>
+        <h4>signup successfull</h4>
+    </div>
+  )
+}
